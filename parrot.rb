735aa8ab67288d5288e4e257af92e579b919279a
Hailey Bongo <haileyebongo@gmail.com>
2020-01-24 03:33:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string)
-  puts #{string}
-  return string
+def parrot(phrase)
+  puts #{phrase}
+  return phrase
 end
